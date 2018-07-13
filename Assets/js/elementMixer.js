ddeb9f6c25d1be255333457f088f9fcd4570b44d
Mário Gil <mg_mesquita@hotmail.com>
2018-07-13 11:16:34 +0100

    Removing previously selected elements.

--- Assets/js/elementMixer.js
+++ Assets/js/elementMixer.js
@@ -1,18 +1,22 @@
 var elementLinks = {
 	questionMark: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Question_Mark.svg/2000px-Question_Mark.svg.png",
 	potato: "http://carismapotatoes.ca/wp-content/uploads/2016/09/potato.png"
-}
+};
 
 $(".row").on("click", "div", function() {
 	if ($("#firstElement").hasClass("questionMark")) {
 		var clickedElement = $(this).children("img").attr("class");
-		$("#firstElement").removeClass("questionMark");
-		$("#firstElement").addClass(clickedElement);
+		$("#firstElement").attr("class", clickedElement);
 	} else {
 		if ($("#secondElement").hasClass("questionMark")) {
 			var clickedElement = $(this).children("img").attr("class");
-			$("#secondElement").removeClass("questionMark");
-			$("#secondElement").addClass(clickedElement);
+			$("#secondElement").attr("class", clickedElement);
 		}
 	}
-})
+});
+
+$("#formula span").on("click", function() {
+	if(!($(this).children("img").hasClass("questionMark"))) {
+		$(this).children("img").attr("class", "questionMark");
+	}
+});
